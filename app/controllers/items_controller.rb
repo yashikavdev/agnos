20104f33fb4b7525114df2b70d92e6c4d7ef5fa0
Yashika <yvijayvargiyadev@gmail.com>
2023-06-05 23:08:16 +0530

    Added Item resource

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,21 @@
+class ItemsController < ApplicationController
+  def index
+    render json: Item.all
+  end
+
+  def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: item, status: :created
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def item_params
+      params.require(:item).permit(:name, :price, :description)
+    end
+end
